29ce033f0e8de2acf82be089aa664d778a4095b0
SammiLodewyckxPXL <148972106+SammiLodewyckxPXL@users.noreply.github.com>
2025-05-11 22:23:14 +0200

    .

--- Backend/Azul.Api/Services/Contracts/ITokenFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Azul.Core.UserAggregate;
-
-namespace Azul.Api.Services.Contracts;
-//DO NOT TOUCH THIS FILE!!
-
-public interface ITokenFactory
-{
-    string CreateToken(User user, IList<string> roleNames);
-}
